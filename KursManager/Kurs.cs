fad9dccdab98a22cab102504ff5ced27c1db1319
Anett Hübner <anett.huebner@starwit.de>
2024-09-26 13:23:22 +0200

    TeilnehmerList statt teilnehmerList

--- KursManager/Kurs.cs
+++ KursManager/Kurs.cs
@@ -5,21 +5,18 @@
         private string? name;
         private string? trainer;
         private DateOnly[]? dates;
-        private List<Teilnehmer>? teilnehmerList;
+        private List<Teilnehmer> teilnehmerList;
 
         public string? Name { get; set; }
         public string? Trainer { get; set; }
         public DateOnly[]? Dates { get; set; }
-        public List<Teilnehmer> TeilnehmerList {
-            get {
-                if (teilnehmerList == null)
-                {
-                    teilnehmerList = new List<Teilnehmer>();
-                }
-                return teilnehmerList;
-            } 
-            set { teilnehmerList = value; }
-            }
+        public List<Teilnehmer> TeilnehmerList { get; set; }
+
+
+        public Kurs()
+        {
+            TeilnehmerList = new List<Teilnehmer>();
+        }
 
         public override string ToString()
         {
@@ -35,12 +32,12 @@
                 } 
             }
             string teilnehmerString = "Teilnehmer: " + "\n\t    ";
-            if (teilnehmerList != null)
+            if (TeilnehmerList != null)
             {
-                int count = teilnehmerList.Count;
+                int count = TeilnehmerList.Count;
                 for (int i = 0; i < count; i++)
                 {
-                    teilnehmerString = teilnehmerString + teilnehmerList[i].ToString() + "\n\t    ";
+                    teilnehmerString = teilnehmerString + TeilnehmerList[i].ToString() + "\n\t    ";
                 }
             }
             return "Name: " + Name + "\n\t" 
